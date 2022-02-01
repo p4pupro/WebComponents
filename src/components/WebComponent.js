f51778ac618cf6bf78d5795e29131fcd6e530686
p4pupro <domingoperezguerrero@gmail.com>
2022-02-01 22:31:29 +0000

    boilerplate complete!

---
+++ src/components/WebComponent.js
@@ -0,0 +1,31 @@
+
+class WebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  static get styles() {
+    return /* css */`
+      .container {
+        width: var(--width);
+        height: var(--height);
+        position: relative;
+      }
+    `;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = /* html */`
+    <style>${WebComponent.styles}</style>
+    <div class="container">
+      <h1>Welcome to WebComponents Boilerplate</h1>
+    </div>`;
+  }
+}
+
+customElements.define("web-component", WebComponent);
